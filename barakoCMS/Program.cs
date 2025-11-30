505c5cd660e90ee7bd689f9c0fe2fd66c551dbb2
arnel <arnelirobles@gmail.com>
2025-11-30 12:42:17 +0800

    Refactor startup logic to use extension methods

--- barakoCMS/Program.cs
+++ barakoCMS/Program.cs
@@ -1,51 +1,19 @@
-using FastEndpoints;
-using FastEndpoints.Swagger;
-using FastEndpoints.Security;
-using Marten;
-using Marten.Events.Projections;
-using Weasel.Core;
-using barakoCMS.Models;
+using barakoCMS.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddFastEndpoints();
-builder.Services.SwaggerDocument();
-builder.Services.AddHealthChecks();
-
-builder.Services.AddJWTBearerAuth(builder.Configuration["JWT:Key"]!);
-builder.Services.AddAuthorization();
-builder.Services.AddScoped<barakoCMS.Repository.IUserRepository, barakoCMS.Repository.MartenUserRepository>();
-
-var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
-
-builder.Services.AddMarten((StoreOptions options) =>
-{
-    options.Connection(connectionString!);
-    
-
-
-    // Configure document versioning
-    options.Schema.For<Content>().DocumentAlias("contents");
-    options.Schema.For<User>().DocumentAlias("users");
-
-    options.Projections.Snapshot<Content>(SnapshotLifecycle.Inline);
-});
+builder.Services.AddBarakoCMS(builder.Configuration);
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-app.UseAuthentication();
-app.UseAuthorization();
-app.UseFastEndpoints(c => 
-{
-    c.Errors.UseProblemDetails();
-});
-app.UseSwaggerGen();
+app.UseBarakoCMS();
 
 app.MapHealthChecks("/health");
 
 await barakoCMS.Data.DataSeeder.SeedAsync(app);
 
 app.Run();
+public partial class Program { }
 public partial class Program { }
